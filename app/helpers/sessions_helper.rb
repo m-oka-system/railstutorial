11a55cbc9c14143acbffdc9c5a0c7bfc3d20313c
Makoto Okada <fun-for-fan@outlook.com>
2018-02-08 22:12:27 +0900

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,10 +4,16 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # 永続セッションとしてユーザーを記憶する
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # 渡されたユーザーがログイン済みユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
   end
 
   # 現在ログイン中のユーザーを返す（いる場合）
@@ -39,5 +45,16 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  # 記憶したURL (もしくはデフォルト値) にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:fowarding_url] || default)
+    session.delete(:fowarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:fowarding_url] = request.original_url if request.get?
   end
 end
