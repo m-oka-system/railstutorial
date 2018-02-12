50c67b5a86202e4b739f3297a5cc222773037fbe
Makoto Okada <fun-for-fan@outlook.com>
2018-02-12 23:17:18 +0900

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,6 +42,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     redirect_to root_url and return unless @user.activated?
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def destroy
@@ -56,14 +57,7 @@ private
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
-  # ログイン済みユーザーかどうか確認
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
+  # beforeフィルタ
 
   # 正しいユーザーかどうか確認
   def correct_user
